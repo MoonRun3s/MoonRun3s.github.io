80c15128540d8ce80e7802f5b95ffa79e99a1b10
jrich <68448908+MoonRun3s@users.noreply.github.com>
2024-09-18 18:38:31 -0400

    Added theme storage

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,19 +1,32 @@
+function getTheme() {
+    return localStorage.getItem('theme') || 'light';
+}
+
+function saveTheme(theme) {
+    localStorage.setItem('theme', theme);
+}
+
+function applyTheme(theme) {
+    if (theme === "dark") {
+        r.style.setProperty("--white", "#F2EFE7");
+        r.style.setProperty("--black", "#121212");
+        r.style.setProperty("--orange", "#f37521");
+    } else {
+        r.style.setProperty("--white", "#121212");
+        r.style.setProperty("--black", "#F2EFE7");
+        r.style.setProperty("--orange", "#f33321");
+    }
+}
+
 window.addEventListener("DOMContentLoaded", event => {
     var r = document.querySelector(":root");
-    var dark = false;
-    const theme = document.getElementById("theme");
-    theme.addEventListener("click", event => {
-        if (dark) {
-            r.style.setProperty("--white", "#F2EFE7");
-            r.style.setProperty("--black", "#121212");
-            r.style.setProperty("--orange", "#f37521");
-            dark = false;
-        } else {
-            r.style.setProperty("--white", "#121212");
-            r.style.setProperty("--black", "#F2EFE7");
-            r.style.setProperty("--orange", "#f33321");
-            dark = true;
-        }
+    var theme = getTheme();
+    applyTheme(theme);
+    const themeButton = document.getElementById("theme");
+    themeButton.addEventListener("click", event => {
+        theme = theme === "dark" ? "light" : "dark";
+        applyTheme(theme);
+        saveTheme(theme);
         return false;
     });
 });
